8fa4ef1de81e04df97b8174f1d2c982f02669c2b
Mark Fynes <tfynes@pivotal.io>
2019-09-27 18:12:34 +0000

    updated decrypt env var for PAS27+ compatibility

--- DirectorPassword/directorPassword.sh
+++ DirectorPassword/directorPassword.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo -u tempest-web /home/tempest-web/tempest/web/scripts/decrypt /var/tempest/workspaces/default/actual-installation.yml  /tmp/ai.yml 2>/dev/null
+sudo -u tempest-web SECRET_KEY_BASE=X /home/tempest-web/tempest/web/scripts/decrypt /var/tempest/workspaces/default/actual-installation.yml  /tmp/ai.yml 2>/dev/null
 cat /tmp/ai.yml | grep -m 1 -A 1 "identity: director" | grep password | awk '{print $2}'
 sudo rm -f /tmp/ai.yml
